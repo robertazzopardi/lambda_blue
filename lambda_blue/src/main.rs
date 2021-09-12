e448cd11bb995aaff852ce283117e55daae6797b
robertazzopardi <robertazzopardi@icloud.com>
2021-09-12 19:35:26 +0100

    Included some handy macros

--- lambda_blue/src/main.rs
+++ lambda_blue/src/main.rs
@@ -4,6 +4,7 @@ const EXIT_TEXT: &str = "Exit";
 const EMULATORS: [&str; 2] = [chip8::emulator_driver::NAME, EXIT_TEXT];
 // const COUNT: usize = EMULATORS.len();
 
+#[macro_use]
 mod text_font {
     use sdl2::{
         pixels::Color,
@@ -13,22 +14,28 @@ mod text_font {
         video::WindowContext,
     };
 
-    use crate::window::{HEIGHT, WIDTH};
+    use crate::window::get_centered_rect;
 
-    pub struct Text<'a> {
-        pub texture: Texture<'a>,
-        pub target: Rect,
-        pub text: &'a str,
+    macro_rules! text_list {
+        ($font:expr, $texture_creator:expr, $names:expr) => {
+            $names
+                .iter()
+                .enumerate()
+                .map(|(index, name)| {
+                    Text::new(*name, &$font, &$texture_creator, 0, (index * 80) as u32).unwrap()
+                })
+                .collect()
+        };
     }
 
-    // handle the annoying Rect i32
-    macro_rules! rect(
-        ($x:expr, $y:expr, $w:expr, $h:expr) => (
-            Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
-        )
-    );
+    pub struct Text<'a> {
+        texture: Texture<'a>,
+        target: Rect,
+        text: &'a str,
+    }
 
     impl<'a> Text<'_> {
+        /// Get a new text object
         pub fn new(
             text: &'a str,
             font: &Font,
@@ -50,38 +57,25 @@ mod text_font {
             // If the example text is too big for the screen, downscale it (and center irregardless)
             Ok(Text {
                 texture,
-                target: get_centered_rect(width, height, WIDTH, HEIGHT, x, y),
+                target: get_centered_rect(width, height, x, y),
                 text,
             })
         }
-    }
 
-    // Scale fonts to a reasonable size when they're too big (though they might look less smooth)
-    fn get_centered_rect(
-        rect_width: u32,
-        rect_height: u32,
-        cons_width: u32,
-        cons_height: u32,
-        x: u32,
-        y: u32,
-    ) -> Rect {
-        let wr = rect_width as f32 / cons_width as f32;
-        let hr = rect_height as f32 / cons_height as f32;
+        /// Get a reference to the text's texture.
+        pub fn texture(&self) -> &Texture<'_> {
+            &self.texture
+        }
 
-        let (w, h) = if wr > 1f32 || hr > 1f32 {
-            println!("Scaling down! The text will look worse!");
-            if wr > hr {
-                let h = (rect_height as f32 / wr) as u32;
-                (cons_width, h)
-            } else {
-                let w = (rect_width as f32 / hr) as u32;
-                (w, cons_height)
-            }
-        } else {
-            (rect_width, rect_height)
-        };
+        /// Get a reference to the text's target.
+        pub fn target(&self) -> Rect {
+            self.target
+        }
 
-        rect!(x, y, w, h)
+        /// Get a reference to the text's text.
+        pub fn text(&self) -> &str {
+            self.text
+        }
     }
 }
 
@@ -118,11 +112,21 @@ mod file_system {
 }
 
 mod window {
-    use crate::{emulator::EmulatorAndRom, file_system, text_font::Text, EMULATORS, EXIT_TEXT};
-    use sdl2::{event::Event, keyboard::Keycode, rect::Point, ttf::Font};
-    use std::path::Path;
-
     extern crate sdl2;
+    use crate::{emulator::EmulatorAndRom, file_system, text_font::Text, EMULATORS, EXIT_TEXT};
+    use sdl2::{
+        event::Event,
+        keyboard::Keycode,
+        rect::{Point, Rect},
+        ttf::Font,
+    };
+
+    // handle the annoying Rect i32
+    macro_rules! rect(
+        ($x:expr, $y:expr, $w:expr, $h:expr) => (
+            Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
+        )
+    );
 
     pub const WIDTH: u32 = 600;
     pub const HEIGHT: u32 = 600;
@@ -130,13 +134,15 @@ mod window {
     macro_rules! draw_text_list {
         ($list:expr, $canvas:expr) => {
             for text in $list.iter() {
-                $canvas.copy(&text.texture, None, Some(text.target))?;
+                $canvas.copy(&text.texture(), None, Some(text.target()))?;
             }
         };
     }
 
     pub fn main_window() -> Result<Option<EmulatorAndRom>, String> {
         let sdl_context = sdl2::init()?;
+        let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
+        let mut events = sdl_context.event_pump()?;
 
         let window = sdl_context
             .video()?
@@ -144,8 +150,6 @@ mod window {
             .position_centered()
             .build()
             .map_err(|e| e.to_string())?;
-
-        let mut events = sdl_context.event_pump()?;
 
         let mut canvas = window
             .into_canvas()
@@ -156,26 +160,13 @@ mod window {
 
         let texture_creator = canvas.texture_creator();
         // Load a font
-        let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
-        let path: &Path = Path::new("lambda_blue/fonts/open-sans/OpenSans-ExtraBold.ttf");
-        let mut font: Font = ttf_context.load_font(path, 56)?;
-        font.set_style(sdl2::ttf::FontStyle::BOLD);
+        let font: Font =
+            ttf_context.load_font("lambda_blue/fonts/open-sans/OpenSans-ExtraBold.ttf", 56)?;
 
-        let emulator_names: Vec<Text> = EMULATORS
-            .iter()
-            .enumerate()
-            .map(|(index, name)| {
-                Text::new(*name, &font, &texture_creator, 0, (index * 80) as u32).unwrap()
-            })
-            .collect();
+        let emulator_names: Vec<Text> = text_list!(font, texture_creator, EMULATORS);
 
-        let no_roms_text_vec: Vec<Text> = vec!["Load Rom", "Back"]
-            .iter()
-            .enumerate()
-            .map(|(index, name)| {
-                Text::new(*name, &font, &texture_creator, 0, (index * 80) as u32).unwrap()
-            })
-            .collect();
+        let no_roms_text_vec: Vec<Text> =
+            text_list!(font, texture_creator, vec!["Load Rom", "Back"]);
 
         let rom_names: Option<Vec<Text>> = None;
 
@@ -195,26 +186,26 @@ mod window {
 
                         if emulator.is_empty() {
                             for name in emulator_names.iter() {
-                                if name.target.contains_point(mouse_pos) {
-                                    println!("{}", name.text);
-                                    if name.text == EXIT_TEXT {
+                                if name.target().contains_point(mouse_pos) {
+                                    println!("{}", name.text());
+                                    if name.text() == EXIT_TEXT {
                                         break 'running;
                                     }
-                                    emulator = name.text.to_string();
+                                    emulator = name.text().to_string().clone();
                                 }
                             }
                         } else {
                             for name in no_roms_text_vec.iter() {
-                                if name.target.contains_point(mouse_pos) {
-                                    println!("{} {:?}", name.text, home::home_dir());
+                                if name.target().contains_point(mouse_pos) {
+                                    println!("{} {:?}", name.text(), home::home_dir());
 
-                                    if name.text == "Back" {
+                                    if name.text() == "Back" {
                                         emulator = String::new();
                                         break;
                                     }
 
-                                    if let Ok(file) = file_system::choose_file() {
-                                        rom = file;
+                                    if let Ok(file_name) = file_system::choose_file() {
+                                        rom = file_name.clone();
                                         break 'running;
                                     }
                                 }
@@ -241,7 +232,29 @@ mod window {
         if !emulator.is_empty() && !rom.is_empty() {
             return Ok(Some(EmulatorAndRom::new(emulator, rom)));
         }
+
         Ok(None)
+    }
+
+    // Scale fonts to a reasonable size when they're too big (though they might look less smooth)
+    pub fn get_centered_rect(rect_width: u32, rect_height: u32, x: u32, y: u32) -> Rect {
+        let wr = rect_width as f32 / WIDTH as f32;
+        let hr = rect_height as f32 / HEIGHT as f32;
+
+        let (w, h) = if wr > 1f32 || hr > 1f32 {
+            println!("Scaling down! The text will look worse!");
+            if wr > hr {
+                let h = (rect_height as f32 / wr) as u32;
+                (WIDTH, h)
+            } else {
+                let w = (rect_width as f32 / hr) as u32;
+                (w, HEIGHT)
+            }
+        } else {
+            (rect_width, rect_height)
+        };
+
+        rect!(x, y, w, h)
     }
 }
 
